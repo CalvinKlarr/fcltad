449a491cdba1ad2fed126ce96f26985a7c4699f9
Juan Andres Calvo <juan_andres_calvo@yahoo.com.ar>
2019-06-06 02:36:56 -0300

    Utils y programcontrol

---
+++ Facultad/Utils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facultad
+{
+    static class Utils
+    {
+        public static bool checkCorrel(List<string> correlativas, Alumno alumno)
+        {
+            int num = correlativas.Count, flag = 0;
+            foreach (var cor in correlativas)
+            {
+                foreach (var apr in alumno.aprobadas)
+                {
+                    if (apr.Equals(cor)) {
+                        flag++;
+                    }
+                }
+            }
+            if (flag == num)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
